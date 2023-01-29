5ca0b422c386b13fac0c76a7bee018d1458274f3
kapil532 <kapil.bic@gmail.com>
2023-01-29 16:18:25 +0530

    screen layout2

--- app/src/main/java/com/lawyaar/MainActivity.kt
+++ app/src/main/java/com/lawyaar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lawyaar
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
@@ -11,24 +12,32 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.lawyaar.auth.PhoneActivity
 import com.lawyaar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        init()
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "search", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            if (auth.currentUser != null){
+                auth.signOut()
+                startActivity(Intent(this , PhoneActivity::class.java))
+                finish()
+            }
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -44,7 +53,11 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
+    private fun init()
+    {
 
+        auth = FirebaseAuth.getInstance()
+    }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
