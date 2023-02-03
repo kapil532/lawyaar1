e985a496346a8abbb22c11362e135204a2a7cc54
kapil532 <kapil.bic@gmail.com>
2023-02-03 16:50:12 +0530

    apply filter

---
+++ app/src/main/java/com/lawyaar/ui/profile/UpdateProfileActivity.kt
@@ -0,0 +1,36 @@
+package com.lawyaar.ui.profile
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.lawyaar.R
+import com.lawyaar.ui.base_screen.BaseActivity
+
+class UpdateProfileActivity : BaseActivity()
+{
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.update_profile_screen)
+
+
+        val back_icon = findViewById<ImageView>(R.id.back_icon)
+        back_icon.setOnClickListener({
+            finish()
+        })
+
+
+        val user_name =  findViewById<TextView>(R.id.user_name)
+        val update_user_name =  findViewById<TextInputEditText>(R.id.update_user_name)
+        val user_mobileno =  findViewById<TextInputEditText>(R.id.user_mobileno)
+        val user_email =  findViewById<TextInputEditText>(R.id.user_email)
+    }
+
+
+
+
+
+}
