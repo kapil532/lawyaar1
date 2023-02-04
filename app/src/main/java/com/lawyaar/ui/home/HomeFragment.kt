df068449da7820b79263f7a555f055b5fbbb8ac1
kapil532 <kapil.bic@gmail.com>
2023-02-04 16:38:26 +0530

    Shimmer effect

--- app/src/main/java/com/lawyaar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lawyaar/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.lawyaar.ui.home
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerFrameLayout
 import com.lawyaar.MainActivity
 import com.lawyaar.R
 import com.lawyaar.adapters.LawyaarAdapter
@@ -35,11 +38,12 @@ class HomeFragment : Fragment(), CellClickListener, TalkListner {
 
     private var layoutManager: RecyclerView.LayoutManager? = null
     private lateinit var adapter: LawyaarAdapter
-
     lateinit var homeViewModel: HomeViewModel
 
     @Inject
     lateinit var homeViewModelFactory: HomeViewModelFactory
+    lateinit var shimmer_view_container: ShimmerFrameLayout
+
 
     @SuppressLint("FragmentLiveDataObserve")
     override fun onCreateView(
@@ -48,8 +52,9 @@ class HomeFragment : Fragment(), CellClickListener, TalkListner {
         savedInstanceState: Bundle?
     ): View {
 
-      val veiw = inflater.inflate(R.layout.fragment_home, container, false)
+        val veiw = inflater.inflate(R.layout.fragment_home, container, false)
         var recycle_veiw = veiw.findViewById<RecyclerView>(R.id.recycle_veiw)
+        shimmer_view_container = veiw.findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container)
         recycle_veiw.layoutManager = LinearLayoutManager(activity)
         adapter = LawyaarAdapter()
         recycle_veiw.adapter = adapter
@@ -59,26 +64,19 @@ class HomeFragment : Fragment(), CellClickListener, TalkListner {
     }
 
     @SuppressLint("FragmentLiveDataObserve")
-    fun initNetwork()
-    {
+    fun initNetwork() {
         (activity?.application as LawyaarApplication).applicationComponent.inject(homeFragment = this)
-        homeViewModel = ViewModelProvider(this,homeViewModelFactory).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this, homeViewModelFactory).get(HomeViewModel::class.java)
 
         homeViewModel.quotes.observe(this, Observer<QuoteList> {
-            if (it != null)
-            {
+            if (it != null) {
+                shimmer_view_container.visibility = View.GONE
                 adapter.setUpdateData(it.results)
             }
         })
 
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 
     override fun onCellClickListener() {
         startActivity(Intent(context, LawyaarDetailsActivity::class.java))
