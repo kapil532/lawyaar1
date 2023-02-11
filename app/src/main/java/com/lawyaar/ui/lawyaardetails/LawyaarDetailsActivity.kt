2411c0ce0f271c6601ea4b57bea9fe776ad66196
kapil532 <kapil.bic@gmail.com>
2023-02-11 17:27:07 +0530

    veiw

--- app/src/main/java/com/lawyaar/ui/lawyaardetails/LawyaarDetailsActivity.kt
+++ app/src/main/java/com/lawyaar/ui/lawyaardetails/LawyaarDetailsActivity.kt
@@ -5,17 +5,19 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.lawyaar.R
+import com.lawyaar.databinding.ActivityMainBinding
 
 class LawyaarDetailsActivity : AppCompatActivity()
 {
 
-
-    @SuppressLint("SuspiciousIndentation")
+  //lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-             setContentView(R.layout.lawyer_profile_layout)
+      //  binding = ActivityMainBinding.inflate(layoutInflater)
+       // val view = binding.root
+        setContentView(R.layout.lawyer_profile_layout)
 
-         // to back the past activity
+         // to bazsck the past activity
         val back_icon = findViewById<ImageView>(R.id.back_icon)
                  back_icon.setOnClickListener({
                       finish()
