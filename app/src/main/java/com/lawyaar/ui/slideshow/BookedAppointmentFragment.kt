9c79bf7da7dd1be9548a99ebf46468be6b008922
kapil532 <kapil.bic@gmail.com>
2023-03-02 23:56:02 +0530

    booking_appointment1

--- app/src/main/java/com/lawyaar/ui/slideshow/BookedAppointmentFragment.kt
+++ app/src/main/java/com/lawyaar/ui/slideshow/BookedAppointmentFragment.kt
@@ -3,10 +3,13 @@ package com.lawyaar.ui.slideshow
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.ShimmerFrameLayout
@@ -14,10 +17,24 @@ import com.lawyaar.R
 import com.lawyaar.adapters.BookedSessionAdaptar
 import com.lawyaar.adapters.LawyaarAdapter
 import com.lawyaar.application.LawyaarApplication
+import com.lawyaar.models.book_session.view_model.BookingSessionFactoryModel
+import com.lawyaar.models.booked_session.BookedSessionModel
+import com.lawyaar.models.booked_session.view_models.BookedSessionFactoryModel
+import com.lawyaar.models.booked_session.view_models.BookedSessionViewModel
+import com.lawyaar.models.lawyer_search.view_model.LawyerSearchFactoyModel
+import com.lawyaar.models.lawyer_search.view_model.LawyerSearchViewModel
+import javax.inject.Inject
 
 class BookedAppointmentFragment : Fragment() {
     lateinit var shimmer_view_container: ShimmerFrameLayout
     private lateinit var adapter: BookedSessionAdaptar
+
+
+    lateinit var bookedSessionViewModel: BookedSessionViewModel
+
+    @Inject
+    lateinit var bookedSessionFactoryModel: BookedSessionFactoryModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,10 +57,23 @@ class BookedAppointmentFragment : Fragment() {
         val sharedPreferences: SharedPreferences =
             activity?.application!!.getSharedPreferences("token_auth", Context.MODE_PRIVATE)
         tokenValue = sharedPreferences.getString("token_val", " ").toString()
-
+        user_id = sharedPreferences.getString("user_id", " ").toString()
         (activity?.application as LawyaarApplication).applicationComponent.inject(bookedAppointmentFragment=this)
 
-
+        bookedSessionViewModel =
+            ViewModelProvider(this, bookedSessionFactoryModel).get(BookedSessionViewModel::class.java)
+        bookedSessionViewModel.getBookedSession(tokenValue,"language,case,category,location",user_id,"3-3-2023")
+        bookedSessionViewModel.bookedSessionLD.observe(viewLifecycleOwner, Observer<BookedSessionModel>{
+            if (it != null) {
+                shimmer_view_container.hideShimmer()
+                shimmer_view_container.visibility = View.GONE
+                adapter.setUpdateData(it)
+                Log.d("NOFOUND","NO LAWWAY"+it)
+            }
+            else{
+                Log.d("NOFOUND","NO LAWWAY")
+            }
+        })
     }
     override fun onDestroyView() {
         super.onDestroyView()
