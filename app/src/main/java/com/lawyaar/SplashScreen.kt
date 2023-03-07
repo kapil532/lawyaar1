4fc4f25416c30fc87ebc35dd3ddf9fbc7f1e8281
kapil532 <kapil.bic@gmail.com>
2023-03-07 17:11:04 +0530

    notification

--- app/src/main/java/com/lawyaar/SplashScreen.kt
+++ app/src/main/java/com/lawyaar/SplashScreen.kt
@@ -14,9 +14,8 @@ import com.lawyaar.ui.auth.OTPActivity
 import com.lawyaar.ui.auth.PhoneActivity
 import com.lawyaar.ui.intro_screen.IntroScreen
 
-class SplashScreen : AppCompatActivity()
-{
-    private lateinit var auth : FirebaseAuth
+class SplashScreen : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,43 +24,40 @@ class SplashScreen : AppCompatActivity()
 
     }
 
-    private fun init()
-    {
+    private fun init() {
 
         auth = FirebaseAuth.getInstance()
-        Handler(Looper.getMainLooper()).postDelayed({
-            checkvalidity()
-        }, 3000)
+        checkvalidity()
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            checkvalidity()
+//        }, 3000)
     }
 
     @SuppressLint("SuspiciousIndentation")
-    private fun checkvalidity()
-    {
-        val sharedPreferences: SharedPreferences = this.getSharedPreferences("token_auth", Context.MODE_PRIVATE)
+    private fun checkvalidity() {
+        val sharedPreferences: SharedPreferences =
+            this.getSharedPreferences("token_auth", Context.MODE_PRIVATE)
 
-       val tokenValue = sharedPreferences.getString("token_val"," ")
-         Log.d("TOKEN VALUE","token--> "+tokenValue)
-        if (auth.currentUser != null)
-        {
+        val tokenValue = sharedPreferences.getString("token_val", " ")
+        Log.d("TOKEN VALUE", "token--> " + tokenValue)
+        if (auth.currentUser != null) {
             if (tokenValue != null && tokenValue.length > 10) {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
 
-            }else {
+            } else {
                 val intent = Intent(this, IntroScreen::class.java)
                 startActivity(intent)
                 finish()
             }
-        }
-        else{
+        } else {
             val intent = Intent(this, IntroScreen::class.java)
             startActivity(intent)
             finish()
-       }
+        }
 
 
     }
-
 
 
 }
---
+++ app/src/main/java/com/lawyaar/notification/CloudMessagingService.kt
@@ -0,0 +1,62 @@
+package com.lawyaar.notification
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import android.preference.PreferenceManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.lawyaar.MainActivity
+import com.lawyaar.R
+
+class CloudMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        updateToken(token)
+    }
+
+    private fun updateToken(newToken: String?) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        preferences.edit().putString("device_token", newToken).apply()
+        Log.d("TOKEN","token--> "+newToken)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage)
+    {
+       Log.d("MSG",""+ remoteMessage.notification!!.body)
+
+        val notificationTitle = remoteMessage.notification!!.title
+        val notificationBody = remoteMessage.notification!!.body
+
+        showNotification(notificationTitle, notificationBody)
+    }
+
+    fun showNotification(notificationTitle: String?, notificationBody: String?)
+    {
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, getString(R.string.appointment_notifications_channel_id))
+                .setAutoCancel(true) // Automatically delete the notification
+                .setSmallIcon(R.drawable.lawyaar_icon) // Notification icon
+                .setContentIntent(pendingIntent)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationBody)
+                .setSound(defaultSoundUri)
+                as NotificationCompat.Builder
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
