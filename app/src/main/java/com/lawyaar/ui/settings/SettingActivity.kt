971cfd1f4e63134377e4ca45120a56110b825c1c
lalbabusingh <lbsingh043@gmail.com>
2023-07-09 21:55:20 +0530

    Setting page code optimize

--- app/src/main/java/com/lawyaar/ui/settings/SettingActivity.kt
+++ app/src/main/java/com/lawyaar/ui/settings/SettingActivity.kt
@@ -5,30 +5,23 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lawyaar.R
-import com.lawyaar.adapters.PriceAdapter
 import com.lawyaar.adapters.SettingAdapter
 import com.lawyaar.ui.base_screen.BaseActivity
-import com.lawyaar.ui.payment_screen.PaymentActivity
 import com.lawyaar.ui.profile.UpdateProfileActivity
 import com.lawyaar.utils.TalkListner
 
 class SettingActivity : BaseActivity(), TalkListner {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.setting_layout)
-        recyclerPriceSlots = findViewById<RecyclerView>(R.id.prices_list)
-        val back_icon = findViewById<ImageView>(R.id.back_icon)
-        back_icon.setOnClickListener({
+        recyclerPriceSlots = findViewById(R.id.prices_list)
+        val backIcon = findViewById<ImageView>(R.id.back_icon)
+        backIcon.setOnClickListener {
             finish()
-        })
-
+        }
         initRecyle()
     }
 
@@ -44,11 +37,9 @@ class SettingActivity : BaseActivity(), TalkListner {
             "Language",
             "Rate Us",
             "About",
-
-            )
+        )
         val layoutManager = LinearLayoutManager(this)
         recyclerPriceSlots.layoutManager = layoutManager
-
         adapter = SettingAdapter()
         recyclerPriceSlots.adapter = adapter
         adapter.setUplistner(this)
@@ -57,25 +48,21 @@ class SettingActivity : BaseActivity(), TalkListner {
 
     override fun onTalkClickListner(userId: String) {
         when (userId) {
-            "Account"-> updateProfile()
-            "Notifications"-> updateProfile()
-            "Privacy"-> updateProfile()
-            "FAQ"-> updateProfile()
-            "Language"-> updateProfile()
-            "Rate Us"-> openPlayStore()
-            "About"-> updateProfile()
-
-
+            "Account" -> updateProfile()
+            "Notifications" -> updateProfile()
+            "Privacy" -> updateProfile()
+            "FAQ" -> updateProfile()
+            "Language" -> updateProfile()
+            "Rate Us" -> openPlayStore()
+            "About" -> updateProfile()
         }
-
-
     }
-  fun updateProfile()
-  {
-      startActivity(Intent(this@SettingActivity,UpdateProfileActivity::class.java))
-  }
 
-    fun openPlayStore() {
+    private fun updateProfile() {
+        startActivity(Intent(this@SettingActivity, UpdateProfileActivity::class.java))
+    }
+
+    private fun openPlayStore() {
         try {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
         } catch (e: ActivityNotFoundException) {
